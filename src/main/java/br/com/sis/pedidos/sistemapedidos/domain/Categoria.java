2f9568992935a533faf53b40af02e8da3afec46f
Bruno Marques <brunomarques.web@gmail.com>
2018-07-28 22:57:59 -0300

    Mapeamento Objeto Relacional com o JPA na entidade Categoria. Criação Automática da base de dados H2.

--- src/main/java/br/com/sis/pedidos/sistemapedidos/domain/Categoria.java
+++ src/main/java/br/com/sis/pedidos/sistemapedidos/domain/Categoria.java
@@ -3,20 +3,30 @@ package br.com.sis.pedidos.sistemapedidos.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Categoria implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     @Getter
     @Setter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Getter
     @Setter
     private String nome;
+
+    public Categoria() {
+    }
 
     public Categoria(Integer id, String nome) {
         super();
