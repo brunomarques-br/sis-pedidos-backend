dafc63dcaccba2795496d1740aea59a91613ef07
Bruno Marques <brunomarques.web@gmail.com>
2018-07-30 11:30:41 -0300

    Deletando uma categoria com DELETE e Exception BAD_REQUEST para tentativa de exclusão com vínculos.

--- src/main/java/br/com/sis/pedidos/backend/resources/exceptions/ResourcesExceptionHandler.java
+++ src/main/java/br/com/sis/pedidos/backend/resources/exceptions/ResourcesExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.sis.pedidos.backend.resources.exceptions;
 
+import br.com.sis.pedidos.backend.exceptions.DataIntegrityException;
 import br.com.sis.pedidos.backend.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +13,15 @@ import javax.servlet.http.HttpServletRequest;
 public class ResourcesExceptionHandler {
 
     @ExceptionHandler(ObjectNotFoundException.class)
-    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
-
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
         StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
 
+    @ExceptionHandler(DataIntegrityException.class)
+    public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 
 }
--- src/main/java/br/com/sis/pedidos/backend/services/CategoriaService.java
+++ src/main/java/br/com/sis/pedidos/backend/services/CategoriaService.java
@@ -1,9 +1,11 @@
 package br.com.sis.pedidos.backend.services;
 
 import br.com.sis.pedidos.backend.domain.Categoria;
+import br.com.sis.pedidos.backend.exceptions.DataIntegrityException;
 import br.com.sis.pedidos.backend.exceptions.ObjectNotFoundException;
 import br.com.sis.pedidos.backend.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -19,14 +21,23 @@ public class CategoriaService {
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 
-    public Categoria insert(Categoria obj){
+    public Categoria insert(Categoria obj) {
         obj.setId(null);
         return repo.save(obj);
     }
 
-    public Categoria update(Categoria obj){
+    public Categoria update(Categoria obj) {
         find(obj.getId());
         return repo.save(obj);
+    }
+
+    public void delete(Integer id) {
+        find(id);
+        try {
+            repo.deleteById(id);
+        }catch (DataIntegrityViolationException err){
+            throw new DataIntegrityException("Não é possível excluir uma categoria que possua produtos vinculados.");
+        }
     }
 
 }
