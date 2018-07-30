0ee3002e8f39921a0a29a990141cab6fda2f3be6
Bruno Marques <brunomarques.web@gmail.com>
2018-07-30 11:17:11 -0300

    Atualizando Categoria com PUT e pequenos ajustes de melhorias.

--- src/main/java/br/com/sis/pedidos/backend/resources/ProdutoResource.java
+++ src/main/java/br/com/sis/pedidos/backend/resources/ProdutoResource.java
@@ -17,7 +17,7 @@ public class ProdutoResource {
     private ProdutoService service;
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<?> find(@PathVariable Integer id) {
+    public ResponseEntity<Produto> find(@PathVariable Integer id) {
         Produto obj = service.buscar(id);
         return ResponseEntity.ok().body(obj);
     }
