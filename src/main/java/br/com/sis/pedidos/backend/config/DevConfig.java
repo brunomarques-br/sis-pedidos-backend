c5603161ed1a5f6a455116aab9e2778f00ce39ce
Bruno Marques - Java Developer <brunomarques.web@gmail.com>
2018-08-22 08:18:05 -0300

    # Criando o profile de desenvolvimento
    * Adicionando o 'pattern' strategy para controle durante a criação da base de dados.

---
+++ src/main/java/br/com/sis/pedidos/backend/config/DevConfig.java
@@ -0,0 +1,32 @@
+package br.com.sis.pedidos.backend.config;
+
+import br.com.sis.pedidos.backend.services.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DbService dbService;
+
+    /*criando um pattern strategy para controle na criação da base de dados.*/
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+
+        if (!"create".equals(strategy))
+            return false;
+
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}
